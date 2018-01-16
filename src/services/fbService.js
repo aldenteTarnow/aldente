01010606af1f8de21b4c822d316453c3f66a2d2e
stychu <tomaszstysial@o2.pl>
2018-01-16 14:20:53 +0100

    Change FB library

--- src/services/fbService.js
+++ src/services/fbService.js
@@ -1,17 +1,15 @@
-import FB from 'fb';
+import FB from 'fbgraph';
+import Promise from 'bluebird';
 import {facebookApi} from '../config';
 
-FB.options({
-    accessToken: facebookApi.appToken,
-    appId: facebookApi.appId,
-    appSecret: facebookApi.appSecret
-});
+const graph = Promise.promisifyAll(FB);
+
+graph.setAccessTokenAsync(facebookApi.appToken);
+graph.setVersionAsync('2.9');
 
 const batchData = (ids) => {
     const gatherResources = (kind) =>{
-        return FB.api('', 'post', {
-            batch: ids.map(id => ({ method: 'get', relative_url: `${id}/${kind}` }))
-        });
+        return graph.batchAsync(ids.map(id => ({ method: 'get', relative_url: `${id}/${kind}` })));
     };
     const postMetaData = '?fields=story,properties,permalink_url,message,created_time,description,from,link,picture';
     return gatherResources(postMetaData).then((result) => result.map((item) => JSON.parse(item.body)));
@@ -20,7 +18,8 @@ const batchData = (ids) => {
 const getData = () => {
     const url = '293169260696126/feed';
 
-    return FB.api(url).then(({data}) => {
+    return graph.getAsync(url).then(({data}) => {
+        console.log(data);
         const ids = data.map((post) => post.id);
         return batchData(ids);
     });
