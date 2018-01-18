dd0e02ec85814cd4f475ccc7748f9f4cf09cf743
stychu <tomaszstysial@o2.pl>
2018-01-18 02:16:33 +0100

    Fix envs

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
-const googleMapApiKey = process.env.GOOGLE_MAP_API_KEY;
+const googleMapApiKey = process.env.REACT_APP_GOOGLE_MAP_API_KEY;
 const facebookApi = {
-    appId: process.env.FACEBOOK_APP_ID || 146692202789787,
+    appId: process.env.REACT_APP_FACEBOOK_APP_ID || 146692202789787,
     appSecret:
-        process.env.FACEBOOK_APP_SECRET || '1371a7f97f7172c761fed81de438d853',
+        process.env.REACT_APP_FACEBOOK_APP_SECRET || '1371a7f97f7172c761fed81de438d853',
     appToken:
-        process.env.FACEBOOK_APP_TOKEN ||
+        process.env.REACT_APP_FACEBOOK_APP_TOKEN ||
         '146692202789787|DNoUGEJjbCO8LNlE2uU1VSfzVDo'
 };
 
