3b01fadf7de48128dfd04bf77ae0ea4aa47b2e2a
stychu <tomaszstysial@o2.pl>
2018-01-18 01:40:21 +0100

    test color

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ import Menu from './components/menu';
 import Gallery from './components/gallery';
 import Contact from './components/contact';
 import News from './components/news';
-
+import Button from 'material-ui/Button';
 const styles = theme => ({
     root: {
         marginTop: 29,
@@ -31,8 +31,9 @@ const createTheme = (type = 'dark') =>
             type,
             // primary: { main: brown[600] },
             // secondary: { main: green['A700'] },
-            primary: { main: brown[500] }, // Purple and green play nicely together.
-            secondary: { main: green['A700'] } // This is just green.A700 as hex.
+            // primary: { main: brown[500] }, // Purple and green play nicely together.
+            // secondary: { main: green['A700'] } // This is just green.A700 as hex.
+            primary: { light: brown[300], main: brown[600], dark: brown[800], contrastText: '' }
         }
     });
 
@@ -46,52 +47,13 @@ class App extends React.Component {
 
         return (
             <MuiThemeProvider theme={this.state.theme}>
-                <Router>
-                    <Reboot>
-                        <div className={classes.root}>
-                            <Bar
-                                changeTheme={type =>
-                                    this.setState({ theme: createTheme(type) })
-                                }
-                                theme={this.state.theme}
-                            />
-                            <Switch>
-                                <Route exact path="/" component={Welcome} />
-                                <Route
-                                    exact
-                                    strict
-                                    path="/menu/:id?"
-                                    component={Menu}
-                                />
-                                <Route
-                                    exact
-                                    strict
-                                    path="/o-nas"
-                                    component={About}
-                                />
-                                <Route
-                                    exact
-                                    strict
-                                    path="/galeria"
-                                    component={Gallery}
-                                />
-                                <Route
-                                    exact
-                                    strict
-                                    path="/kontakt"
-                                    component={Contact}
-                                />
-                                <Route
-                                    exact
-                                    strict
-                                    path="/nowosci"
-                                    component={News}
-                                />
-                                <Route component={NotFoundPage} />
-                            </Switch>
-                        </div>
-                    </Reboot>
-                </Router>
+                <Reboot>
+                    <div className={classes.root}>
+                        <Button raised color="primary">OK</Button>
+                        <Button raised color="accent">OK</Button>
+                        <Button raised color="inherit">OK</Button>
+                    </div>
+                </Reboot>
             </MuiThemeProvider>
         );
     }
@@ -104,3 +66,109 @@ App.propTypes = {
 const CreateApp = withStyles(styles)(App);
 
 ReactDOM.render(<CreateApp />, document.querySelector('#root'));
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Bar from './components/bar';
+// import Reboot from 'material-ui/Reboot';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import About from './components/about';
+// import { withStyles } from 'material-ui/styles';
+// import PropTypes from 'prop-types';
+// import 'font-awesome/css/font-awesome.min.css';
+// import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+// import brown from 'material-ui/colors/brown';
+// import green from 'material-ui/colors/green';
+// // COMPONENTS
+// import NotFoundPage from './components/notFoundPage';
+// import Welcome from './components/welcome';
+// import Menu from './components/menu';
+// import Gallery from './components/gallery';
+// import Contact from './components/contact';
+// import News from './components/news';
+//
+// const styles = theme => ({
+//     root: {
+//         marginTop: 29,
+//         overflowX: 'hidden',
+//     }
+// });
+//
+// const createTheme = (type = 'dark') =>
+//     createMuiTheme({
+//         palette: {
+//             type,
+//             // primary: { main: brown[600] },
+//             // secondary: { main: green['A700'] },
+//             primary: { main: brown[500] }, // Purple and green play nicely together.
+//             secondary: { main: green['A700'] } // This is just green.A700 as hex.
+//         }
+//     });
+//
+// class App extends React.Component {
+//     state = {
+//         theme: createTheme('dark')
+//     };
+//
+//     render() {
+//         const { classes } = this.props;
+//
+//         return (
+//             <MuiThemeProvider theme={this.state.theme}>
+//                 <Router>
+//                     <Reboot>
+//                         <div className={classes.root}>
+//                             <Bar
+//                                 changeTheme={type =>
+//                                     this.setState({ theme: createTheme(type) })
+//                                 }
+//                                 theme={this.state.theme}
+//                             />
+//                             <Switch>
+//                                 <Route exact path="/" component={Welcome} />
+//                                 <Route
+//                                     exact
+//                                     strict
+//                                     path="/menu/:id?"
+//                                     component={Menu}
+//                                 />
+//                                 <Route
+//                                     exact
+//                                     strict
+//                                     path="/o-nas"
+//                                     component={About}
+//                                 />
+//                                 <Route
+//                                     exact
+//                                     strict
+//                                     path="/galeria"
+//                                     component={Gallery}
+//                                 />
+//                                 <Route
+//                                     exact
+//                                     strict
+//                                     path="/kontakt"
+//                                     component={Contact}
+//                                 />
+//                                 <Route
+//                                     exact
+//                                     strict
+//                                     path="/nowosci"
+//                                     component={News}
+//                                 />
+//                                 <Route component={NotFoundPage} />
+//                             </Switch>
+//                         </div>
+//                     </Reboot>
+//                 </Router>
+//             </MuiThemeProvider>
+//         );
+//     }
+// }
+//
+// App.propTypes = {
+//     classes: PropTypes.object.isRequired
+// };
+//
+// const CreateApp = withStyles(styles)(App);
+//
+// ReactDOM.render(<CreateApp />, document.querySelector('#root'));
